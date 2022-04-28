9f4397bea78457e2e3d45ff88abf795f1746b078
Wilson Brito <wilsonbritoinfo@gmail.com>
2022-04-28 20:30:11 -0300

    Atualizando no GitHub

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -3,4 +3,16 @@
 apt-get update -y
 apt-get upgrade -y
 
+apt-get install apache2
+
+wget https://gist.github.com/nunomazer/873219/archive/690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+apt-get install zip 
+apt-get install unzip
+
+unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
+
+cp cv-completo.html /var/www/html/
+cp cv-resumido.html /var/www/html/
+cp style.css /var/www/html/
 
