aac1807f05ea276fb9d6c52662ab125439dc65e1
Wilson Brito <wilsonbritoinfo@gmail.com>
2022-04-28 22:50:50 -0300

    configura_servidorWeb.sh

--- configura_servidorWeb.sh
+++ configura_servidorWeb.sh
@@ -12,9 +12,14 @@ apt-get install unzip
 
 unzip 690e1f604dc8ead2583589e1aee6f8a42040a035.zip
 
+cd 873219-690e1f604dc8ead2583589e1aee6f8a42040a035
+
 cp cv-completo.html /var/www/html/
 cp cv-resumido.html /var/www/html/
 cp style.css /var/www/html/
 
 rm /var/www/html/index.html
 
+mv /var/www/html/cv-resumido.html /var/www/html/index.html
+
+/etc/init.d/apache2 start
